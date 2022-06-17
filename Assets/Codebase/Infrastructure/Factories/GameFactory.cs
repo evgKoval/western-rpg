a0d40c4f5a11d36b76af2d9d5b55401d521db31b
evgKoval <ekovalwork@gmail.com>
2022-06-17 19:34:53 +0300

    Add aim pose

--- Assets/Codebase/Infrastructure/Factories/GameFactory.cs
+++ Assets/Codebase/Infrastructure/Factories/GameFactory.cs
@@ -3,12 +3,14 @@ using Codebase.Infrastructure.AssetManagement;
 using Codebase.Player;
 using Codebase.Services.Input;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 namespace Codebase.Infrastructure.Factories
 {
   public class GameFactory : IGameFactory
   {
-    private const string LookAt = "Look_At";
+    private const string CameraLookAt = "Look_At";
+    private const string AimLookAt = "Aim Look At";
 
     private readonly IAssetProvider _assetProvider;
     private readonly IInputService _inputService;
@@ -26,6 +28,7 @@ namespace Codebase.Infrastructure.Factories
       _playerGameObject = _assetProvider.Instantiate(AssetPath.Player, at);
 
       _playerGameObject.GetComponent<Movement>().Construct(_inputService);
+      BuildRig();
 
       return _playerGameObject;
     }
@@ -39,9 +42,26 @@ namespace Codebase.Infrastructure.Factories
 
       CinemachineFreeLook cinemachineComponent = playerCamera.GetComponent<CinemachineFreeLook>();
       cinemachineComponent.Follow = _playerGameObject.transform;
-      cinemachineComponent.LookAt = _playerGameObject.transform.Find(LookAt);
+      cinemachineComponent.LookAt = _playerGameObject.transform.Find(CameraLookAt);
 
       return playerCamera;
+    }
+
+    private void BuildRig()
+    {
+      Transform aimLookAt = Camera.main.transform.Find(AimLookAt);
+      
+      foreach (MultiAimConstraint multiAimConstraint in _playerGameObject.GetComponentsInChildren<MultiAimConstraint>())
+        SetSourceObject(multiAimConstraint, aimLookAt);
+
+      _playerGameObject.GetComponentInChildren<RigBuilder>().Build();
+    }
+
+    private static void SetSourceObject(MultiAimConstraint component, Transform aimLookAt)
+    {
+      WeightedTransformArray data = component.data.sourceObjects;
+      data.SetTransform(0, aimLookAt);
+      component.data.sourceObjects = data;
     }
   }
 }
