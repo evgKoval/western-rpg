33033e76b962413dc2a6158435f7d5c98854e1f7
evgKoval <ekovalwork@gmail.com>
2022-06-19 17:53:19 +0300

    Add movement animations

--- Assets/Codebase/Enemy/MoveToPlayer.cs
+++ Assets/Codebase/Enemy/MoveToPlayer.cs
@@ -3,25 +3,32 @@ using UnityEngine.AI;
 
 namespace Codebase.Enemy
 {
-  [RequireComponent(typeof(NavMeshAgent))]
+  [RequireComponent(typeof(NavMeshAgent), typeof(Animator))]
   public class MoveToPlayer : MonoBehaviour
   {
-    [SerializeField] [Range(0, 100)]
-    private float _minimalDistance;
+    private const string Velocity = "Velocity";
+
+    [SerializeField] [Range(0, 100)] private float _minimalDistance;
 
     private NavMeshAgent _agent;
     private Transform _player;
+    private Animator _animator;
 
     public void Construct(Transform player) =>
       _player = player;
 
-    private void Awake() =>
+    private void Awake()
+    {
       _agent = GetComponent<NavMeshAgent>();
+      _animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
       if (_player && IsNotCloseToPlayer())
         _agent.destination = _player.position;
+
+      _animator.SetFloat(Velocity, _agent.velocity.magnitude);
     }
 
     private bool IsNotCloseToPlayer()
