7950ef2a526a5988c9bb962385b88efa7c3b7d88
evgKoval <ekovalwork@gmail.com>
2022-06-20 19:49:53 +0300

    Make an enemy starts attack when he is close enough to the player

---
+++ Assets/Codebase/Logic/TriggerObserver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Codebase.Logic
+{
+  [RequireComponent(typeof(Collider))]
+  public class TriggerObserver : MonoBehaviour
+  {
+    public event Action<Collider> Enter;
+    public event Action<Collider> Exit;
+
+    private void OnTriggerEnter(Collider other) =>
+      Enter?.Invoke(other);
+
+    private void OnTriggerExit(Collider other) =>
+      Exit?.Invoke(other);
+  }
+}
