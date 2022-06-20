0b06ffcdd9d7111018d7acdd00b81c17ecd30d7b
evgKoval <ekovalwork@gmail.com>
2022-06-20 20:18:55 +0300

    Add aggro zone for enemies

---
+++ Assets/Codebase/Enemy/CheckAggroZone.cs
@@ -0,0 +1,36 @@
+﻿using Codebase.Logic;
+using UnityEngine;
+
+namespace Codebase.Enemy
+{
+  [RequireComponent(typeof(MoveToPlayer))]
+  public class CheckAggroZone : MonoBehaviour
+  {
+    [SerializeField] private TriggerObserver _aggroZone;
+
+    private MoveToPlayer _movement;
+
+    private void Awake() =>
+      _movement = GetComponent<MoveToPlayer>();
+
+    private void Start()
+    {
+      _aggroZone.Enter += StartMoving;
+      _aggroZone.Exit += CancelMoving;
+
+      _movement.enabled = false;
+    }
+
+    private void OnDestroy()
+    {
+      _aggroZone.Enter -= StartMoving;
+      _aggroZone.Exit -= CancelMoving;
+    }
+
+    private void StartMoving(Collider obj) =>
+      _movement.enabled = true;
+
+    private void CancelMoving(Collider obj) =>
+      _movement.enabled = false;
+  }
+}
