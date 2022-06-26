1d273824664c65194a83346c22ef655822f87e38
evgKoval <ekovalwork@gmail.com>
2022-06-26 14:56:16 +0300

    Add ability to reload weapon

--- Assets/Codebase/Player/Aiming.cs
+++ Assets/Codebase/Player/Aiming.cs
@@ -6,12 +6,14 @@ using UnityEngine.Animations.Rigging;
 
 namespace Codebase.Player
 {
+  [RequireComponent(typeof(Firing))]
   public class Aiming : MonoBehaviour, IDeathable, IPauseable
   {
     [SerializeField] private Rig _aimingLayer;
     [SerializeField] private float _aimDuration;
 
     private IInputService _inputService;
+    private Firing _firing;
 
     public bool IsReady => Mathf.Approximately(_aimingLayer.weight, 1f);
     public float ReadyPercentage => _aimingLayer.weight;
@@ -21,12 +23,15 @@ namespace Codebase.Player
     public void Construct(IInputService inputService) =>
       _inputService = inputService;
 
+    private void Awake() =>
+      _firing = GetComponent<Firing>();
+
     private void Update()
     {
       if (IsPaused)
         return;
 
-      if (_inputService.IsAimButton())
+      if (_inputService.IsAimButton() && !_firing.IsReloading)
         _aimingLayer.weight += Time.deltaTime / _aimDuration;
       else
         _aimingLayer.weight -= Time.deltaTime / _aimDuration;
--- Assets/Codebase/Player/Firing.cs
+++ Assets/Codebase/Player/Firing.cs
@@ -1,24 +1,40 @@
-﻿using Codebase.Logic;
+﻿using System.Collections;
+using Codebase.Logic;
 using Codebase.Services.Input;
 using Codebase.Services.Pause;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 namespace Codebase.Player
 {
-  [RequireComponent(typeof(Aiming))]
+  [RequireComponent(typeof(Aiming), typeof(Animator))]
   public class Firing : MonoBehaviour, IDeathable, IPauseable
   {
+    private const string ReloadingState = "Reload";
+    private const string RightHand = "Rig_Layer_Hand_IK/Right_Hand_IK";
+
+    [SerializeField] private float _reloadingSpeed;
+
     private IInputService _inputService;
     private Weapon _weapon;
     private Aiming _aiming;
+    private Animator _animator;
+    private TwoBoneIKConstraint _rightHand;
 
     public bool IsPaused { get; private set; }
+    public bool IsReloading { get; private set; }
 
     public void Construct(IInputService inputService) =>
       _inputService = inputService;
 
-    private void Awake() =>
+    private void Awake()
+    {
       _aiming = GetComponent<Aiming>();
+      _animator = GetComponent<Animator>();
+    }
+
+    private void Start() =>
+      _rightHand = transform.Find(RightHand).GetComponent<TwoBoneIKConstraint>();
 
     private void Update()
     {
@@ -26,7 +42,16 @@ namespace Codebase.Player
         return;
 
       if (_inputService.IsFiringButtonDown() && _aiming.IsReady)
+      {
         _weapon.Shoot();
+        StartCoroutine(Reload());
+      }
+    }
+
+    private IEnumerator OnReloadEnded()
+    {
+      yield return RaiseHand();
+      IsReloading = false;
     }
 
     public void EquipWeapon(Weapon weapon) =>
@@ -37,5 +62,34 @@ namespace Codebase.Player
 
     public void Resume() =>
       IsPaused = false;
+
+    private IEnumerator Reload()
+    {
+      IsReloading = true;
+
+      while (!Mathf.Approximately(_aiming.ReadyPercentage, 0))
+        yield return null;
+
+      yield return LowerHand();
+      _animator.SetTrigger(ReloadingState);
+    }
+
+    private IEnumerator LowerHand()
+    {
+      while (!Mathf.Approximately(_rightHand.weight, 0))
+      {
+        _rightHand.weight -= Time.deltaTime / _reloadingSpeed;
+        yield return null;
+      }
+    }
+
+    private IEnumerator RaiseHand()
+    {
+      while (!Mathf.Approximately(_rightHand.weight, 1))
+      {
+        _rightHand.weight += Time.deltaTime / _reloadingSpeed;
+        yield return null;
+      }
+    }
   }
 }
